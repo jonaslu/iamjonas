9b0c3e4a884d3a633dd911de13ab3b20f2d4be0c
Jonas Lundberg <jonasl@iamjonas.me>
2025-02-11 22:27:31 +0100

    Add filter for pretty dates on homepage
    Test plan:
    * task serve, dates looks
      like they used to.

--- eleventy.config.js
+++ eleventy.config.js
@@ -3,7 +3,13 @@ export default async function (eleventyConfig) {
   eleventyConfig.setInputDirectory("src");
 
   eleventyConfig.addPassthroughCopy("src/style.css");
-  
+  eleventyConfig.addFilter("intlPostDate", (date) => new Intl.DateTimeFormat("en-US", {
+    weekday: "long",
+    day: "2-digit",
+    month: "long",
+    year: "numeric",
+  }).format(date));
+
   eleventyConfig.addCollection("posts", (collectionApi) => {
     return collectionApi
       .getFilteredByGlob("src/posts/**/*.md")
