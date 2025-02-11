56e0d036f913a9a0a3451ac4dec94fd0dd710a3e
Jonas Lundberg <jonasl@iamjonas.me>
2025-02-11 22:10:29 +0100

    Add passthrough style.css
    Also this ported over from iamjonas.me
    
    Test plan:
    * task serve and behold
      the index.njk page.
      Verify it now looks less horrible.

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,6 +1,9 @@
 /** @param {import("@11ty/eleventy").UserConfig} eleventyConfig */
 export default async function (eleventyConfig) {
   eleventyConfig.setInputDirectory("src");
+
+  eleventyConfig.addPassthroughCopy("src/style.css");
+  
   eleventyConfig.addCollection("posts", (collectionApi) => {
     return collectionApi
       .getFilteredByGlob("src/posts/**/*.md")
