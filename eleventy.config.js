eed5426d7c768cd187299de8129d796cb8c61b2f
Jonas Lundberg <jonasl@iamjonas.me>
2025-02-13 22:37:43 +0100

    First stab at serving images
    Include the 11ty image plugin
    to do image generation and hook
    it up.
    
    Include an image and style
    it as always centered.
    
    Test plan:
    * task serve and
      reverting unhappiness post
      shows an image at the bottom.

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,18 +1,22 @@
+import { eleventyImageTransformPlugin } from "@11ty/eleventy-img";
+
 /** @param {import("@11ty/eleventy").UserConfig} eleventyConfig */
 export default async function (eleventyConfig) {
   eleventyConfig.setInputDirectory("src");
 
   eleventyConfig.addPassthroughCopy("src/style.css");
-  eleventyConfig.addFilter("intlPostDate", (date) => new Intl.DateTimeFormat("en-US", {
-    weekday: "long",
-    day: "2-digit",
-    month: "long",
-    year: "numeric",
-  }).format(date));
+  eleventyConfig.addFilter("intlPostDate", (date) =>
+    new Intl.DateTimeFormat("en-US", {
+      weekday: "long",
+      day: "2-digit",
+      month: "long",
+      year: "numeric",
+    }).format(date)
+  );
 
   eleventyConfig.addCollection("posts", (collectionApi) => {
-    return collectionApi
-      .getFilteredByGlob("src/posts/**/*.md")
-      .sort((a, b) => b.date - a.date);
+    return collectionApi.getFilteredByGlob("src/posts/**/*.md").sort((a, b) => b.date - a.date);
   });
+
+  eleventyConfig.addPlugin(eleventyImageTransformPlugin);
 }
